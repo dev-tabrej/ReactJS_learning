5c9a24ea67ad79390d52eadd1dde95b5fc842e75
dev-tabrej <dev.tabrej@gmail.com>
2024-02-27 16:14:48 +0530

    Game Finished

---
+++ tic-tac-toe/src/components/Square.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { useState } from 'react';
+
+export default function Square({value, onSquareClick}) {
+    return <button className="columb" onClick={onSquareClick}>{value}</button>;
+  }
