fc362704b62d7e29f0089f86520a3a2579e86897
dev-tabrej <dev.tabrej@gmail.com>
2024-03-04 12:06:49 +0530

    Contacts us form completed

--- contact-us/src/components/Contact.jsx
+++ contact-us/src/components/Contact.jsx
@@ -1,20 +1,52 @@
+import { useState } from 'react'
 import React from 'react'
 
+
 export default function Contact() {
+    let [name, setName] = useState("");
+    let [email, setEmail] = useState("");
+
+    const viaCallSubmit = () => {
+        console.log(`I'm from via call`)
+    }
+    const viaSupport = () => {
+        console.log(`I'm from via Support`)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        setName(event.target[0].value)
+        setEmail(event.target[1].value)
+        console.log(event.target[0].value)
+        console.log(event.target[1].value)
+        console.log(event.target[2].value)
+    }
+
+
     return (
+
         <div className='form-view'>
             <div className="top-buttons">
-                <button className="btn">Via-support chat</button>
-                <button className="btn">via call</button>
+                <button className="btn" onClick={viaSupport}>Via-support chat</button>
+                <button className="btn" onClick={viaCallSubmit}>via call</button>
             </div>
-            <form>
-                <label htmlFor="Name">Name</label>
-                <input type="text" />
-                <label htmlFor="Email">Email</label>
-                <input type="text" />
-                <textarea name="comments" id="" cols="30" rows="10">text</textarea>
+            <button className="btn btn-mail">via Email</button>
+            <form onSubmit={onSubmit}>
+                <div className="form-inputs">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" />
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" />
+                </div>
+                <div className="form-inputs">
+                    <label htmlFor="text">Text</label>
+                    <textarea name="text" />
+                </div>
                 <button className="btn  submit">Submit</button>
             </form>
+            {/* <div >{`${name} is my name and this is my ${email}`}</div> */}
         </div>
     )
 }
